ad31876454caf716d9b690497aa2f2ac3490ec54
David <david.vilchez@tecsup.edu.pe>
2024-10-22 23:19:28 -0500

    Menu correjido pero error en el perfil :c

--- app/src/main/java/com/tecsup/boticaphar/MenuLateralActivity.kt
+++ app/src/main/java/com/tecsup/boticaphar/MenuLateralActivity.kt
@@ -1,19 +1,80 @@
 package com.tecsup.boticaphar
 
-import android.content.Context
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.MenuItem
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import android.widget.ImageView
 
 class MenuLateralActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+
+    private lateinit var drawerLayout: DrawerLayout
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_completo_drawer)
+
+        // Inicializar el DrawerLayout
+        drawerLayout = findViewById(R.id.drawer_menu_perfil_completo)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        val menuIcon: ImageView = findViewById(R.id.menu_icon)
+        menuIcon.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        val navigationView: NavigationView = findViewById(R.id.nav_menu_completo_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_categoria1 -> {
+
+                    true
+                }
+                R.id.nav_categoria2 -> {
+
+                    true
+                }
+                R.id.nav_categoria3 -> {
+
+                    true
+                }
+                R.id.nav_ofertas -> {
+
+                    true
+                }
+                R.id.nav_contactanos -> {
+
+                    true
+                }
+                R.id.nav_sobre_nosotros -> {
+
+                    true
+                }
+
+                else -> false
+            }.also {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/tecsup/boticaphar/PerfilActivity.kt
+++ app/src/main/java/com/tecsup/boticaphar/PerfilActivity.kt
@@ -4,71 +4,115 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
-    class PerfilActivity : AppCompatActivity() {
-        private lateinit var nameTextView: TextView
-        private lateinit var emailTextView: TextView
+class PerfilActivity : AppCompatActivity() {
+    private lateinit var nameTextView: TextView
+    private lateinit var emailTextView: TextView
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_perfil)
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil)
 
-            nameTextView = findViewById(R.id.username)
-            emailTextView = findViewById(R.id.et_email)
+        // Configurar el DrawerLayout y NavigationView
+        drawerLayout = findViewById(R.id.drawer_menu_perfil_completo)
 
+        // Configurar ActionBarDrawerToggle
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-            // Cargar datos de SharedPreferences
-            val sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE)
-            val username = sharedPreferences.getString("username", null)
-            val email = sharedPreferences.getString("email", null)
-            val editarPerfil = findViewById<ImageView>(R.id.ic_profile)
-            val misPedidos = findViewById<ImageView>(R.id.pedidos)
+        // Mostrar el botón de menú en la barra de acción
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-            editarPerfil.setOnClickListener {
-                val intent = Intent(this@PerfilActivity, EditarPerfilActivity::class.java)
-                startActivity(intent)
-            }
-
-            misPedidos.setOnClickListener {
-                val intent = Intent(this@PerfilActivity, HistorialPedidosActivity::class.java)
-                startActivity(intent)
-            }
-
-            // Muestra los detalles en los TextViews
-            nameTextView.text = username ?: "Nombre no disponible"
-            emailTextView.text = email ?: "Email no disponible"
-
-            // Configurar BottomNavigationView si es necesario...
-
-    // Obtén la referencia al BottomNavigationView
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-
-        // Establece el listener para los ítems seleccionados
-        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
+        // Configurar la navegación del menú lateral
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
                 R.id.nav_home -> {
-                    // Navegar a la actividad Home
-                    val intent = Intent(this, HomeActivity::class.java)
-                    startActivity(intent)
+                    startActivity(Intent(this, HomeActivity::class.java))
                     true
                 }
                 R.id.nav_cart -> {
-                    // Navegar a la actividad de carrito
-                    val intent = Intent(this, CarritoActivity::class.java)
-                    startActivity(intent)
+                    startActivity(Intent(this, CarritoActivity::class.java))
                     true
                 }
                 R.id.nav_profile -> {
-                    // Mantente en la actividad actual (Perfil)
+                    drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 else -> false
             }
+        }
+
+        // Configurar BottomNavigationView
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.nav_cart -> {
+                    startActivity(Intent(this, CarritoActivity::class.java))
+                    true
+                }
+                R.id.nav_profile -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Cargar datos de SharedPreferences
+        nameTextView = findViewById(R.id.username)
+        emailTextView = findViewById(R.id.et_email)
+        val sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE)
+        val username = sharedPreferences.getString("username", null)
+        val email = sharedPreferences.getString("email", null)
+        val editarPerfil = findViewById<ImageView>(R.id.ic_profile)
+        val misPedidos = findViewById<ImageView>(R.id.pedidos)
+
+        editarPerfil.setOnClickListener {
+            val intent = Intent(this@PerfilActivity, EditarPerfilActivity::class.java)
+            startActivity(intent)
+        }
+
+        misPedidos.setOnClickListener {
+            val intent = Intent(this@PerfilActivity, HistorialPedidosActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Muestra los detalles en los TextViews
+        nameTextView.text = username ?: "Nombre no disponible"
+        emailTextView.text = email ?: "Email no disponible"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 }
