a26d1da6d970bd6f83d8bf94debf1be271c7dadb
David <david.vilchez@tecsup.edu.pe>
2024-11-22 03:12:57 -0500

    "google maps"

--- app/src/main/java/com/tecsup/boticaphar/PerfilActivity.kt
+++ app/src/main/java/com/tecsup/boticaphar/PerfilActivity.kt
@@ -84,13 +84,14 @@ class PerfilActivity : AppCompatActivity() {
 
     // Función para abrir Google Maps con una ubicación específica
     private fun openGoogleMaps() {
-        val location = Uri.parse("geo:0,0?q=-8.094023264182493,-79.03693931820501") // Coordenadas de FarmaVida
+        val location = Uri.parse("geo:0,0?q=-8.094023264182493,-79.03693931820501&markers=-8.094023264182493,-79.03693931820501|-8.09637611172991,-79.02977049308365|-8.095781285121767,-79.0113168949252|-8.102154382244429,-79.01063024939796")
         val mapIntent = Intent(Intent.ACTION_VIEW, location)
         mapIntent.setPackage("com.google.android.apps.maps")
         if (mapIntent.resolveActivity(packageManager) != null) {
             startActivity(mapIntent)
         }
     }
+
     // Función para cerrar sesión
     private fun cerrarSesion() {
         val sharedPreferences = getSharedPreferences("userPrefs", Context.MODE_PRIVATE)
